3051fe33115d74233bda6fdede83d632cb504fda
Jakub Przybytek <cubanan@gmail.com>
2018-03-11 22:19:43 +0100

    Less flickering for apppendTextLine()

--- DisplaySSD1306/Screen.cpp
+++ DisplaySSD1306/Screen.cpp
@@ -25,7 +25,20 @@ void Screen::drawTextLine(uint8_t lineNumber, const char * textLine) {
 }
 
 void Screen::appendTextLine(const char * textLine) {
-	drawTextLine(currentLineNumber, textLine);
+
+	memset(buffer, 0, 128);
+	sdd1306.setDrawingArea(0, 127, currentLineNumber, currentLineNumber);
+	sdd1306.sendFramebuffer(buffer, 128);
+
 	currentLineNumber = (currentLineNumber + 1 ) % 8;
+
 	sdd1306.setDisplayOffset(currentLineNumber);
+
+	uint8_t col = 0;
+	while (*textLine != '\0') {
+		memcpy(buffer + col, font6x8[(uint8_t) *textLine++ - 32], 6);
+		col += 6;
+	}
+
+	sdd1306.sendFramebuffer(buffer, col);
 }
